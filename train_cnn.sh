cb16f129759e5760ba40258804f936f0828abb88
Danielle LaMay <48498276+dlamay@users.noreply.github.com>
2022-03-14 00:00:33 -0400

    Add files via upload

---
+++ train_cnn.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --time=08:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --job-name=autoencoder_run
+#SBATCH --mem=120GB
+#SBATCH --ntasks=1
+
+module load anaconda3/2022.01
+module load cuda/11.0
+conda create --name TF_env python=3.7 anaconda
+source activate TF_env
+conda install -c anaconda tensorflow-gpu -y
+python train_cnn.py
